bd817526a2bc632ad92376de9f86462251e9e8b5
massimopasteris <44644873+massimopasteris@users.noreply.github.com>
2018-11-06 12:13:32 +0100

    Add files via upload

---
+++ Calcolo.py
@@ -0,0 +1,48 @@
+'''
+Created on 06 nov 2018
+
+@author: mpasteri
+'''
+
+#--------------------------------------------------------
+# #1 - Compute the Euler's number using Newton Expansion.
+# sum(1/(n!) n=0...N
+#--------------------------------------------------------
+
+EulerNumber=1
+for n in range(1,20):  
+    print('----------------------------------------')  
+    print(n)
+    fat=1
+    for ind in range(1,n+1):
+        fat=fat*ind
+    print('fat=',fat)
+    errorE = EulerNumber  
+    EulerNumber=EulerNumber+1/fat
+    errorE=errorE-EulerNumber
+    print(EulerNumber)    
+    print('Error=',errorE)
+    print('----------------------------------------')  
+        
+        
+#--------------------------------------------------------
+# #2 - Brothers' Formulae 
+# EulerNumber= sum( (2*n +2)/(2*n+1)!  ) n=0...N
+# n=0 EulerNumber=2
+#--------------------------------------------------------
+print('---****************************---')  
+
+
+EulerNumber=2
+for n in range(1,6):  
+    print('----------------------------------------')  
+    print(n)
+    fat=1
+    for ind in range(1,(2*n+1)+1 ):
+        fat=fat*ind
+    print('fat=',fat)    
+    EulerNumber=EulerNumber+ 2*(n+1)/fat    
+    print(EulerNumber)    
+    print('----------------------------------------')  
+          
+
